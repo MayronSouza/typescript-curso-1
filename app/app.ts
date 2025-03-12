c69b94f137235e56b0ec83bbec3c424ab7b2421a
MayronSouza <mayron.souza@gmz.solutions>
2025-03-12 17:35:57 -0300

    refatorando de js para ts

---
+++ app/app.ts
@@ -0,0 +1,9 @@
+import { DebtSettlement } from "./models/debt-settlement.js";
+
+const debtSettlement = new DebtSettlement(new Date())
+debtSettlement.quantity = 10
+
+console.log('data da negociação => ', debtSettlement.date)
+console.log('quantidade de negociações => ', debtSettlement.quantity)
+console.log('valor da negociação => ', debtSettlement.value)
+console.log('volume da negociação => ', debtSettlement.volume)
